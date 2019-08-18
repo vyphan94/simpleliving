f2b64f42b835f045f3bacdf73c6cd8a9fdc70f78
Duong Le <Kayz@duongs-mbp.lan>
2019-08-18 14:56:24 -0400

    initial commit

---
+++ assets/js/jsdefault.js
@@ -0,0 +1,15 @@
+//Search Desktop
+function openNavSearch() {
+    document.getElementById("searchSL").style.top = "0";
+}
+function closeNavSearch() {
+    document.getElementById("searchSL").style.top = "-100px";
+}
+
+//sidenav Mobile
+function openSidenavSL() {
+    document.getElementById("sidenavSL").style.left = "0";
+}
+function closeSidenavSL() {
+    document.getElementById("sidenavSL").style.left = "-100%";
+}
